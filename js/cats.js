33a2d3de1115bd1acc0c00c6607a29a9c15a7027
莫开车 <1357953389@qq.com>
2026-07-23 21:24:14 +0800

    chore: 增补大头照片（datou11）

--- js/cats.js
+++ js/cats.js
@@ -305,7 +305,7 @@ const catProfiles = [
     notes: '—',
     area: '丁香',
     gender: '公',
-    images: ['images/大头/datou10.jpg', 'images/大头/datou9.jpg', 'images/大头/datou8.jpg', 'images/大头/datou7.jpg', 'images/大头/datou6.jpg', 'images/大头/datou5.jpg', 'images/大头/datou4.jpg', 'images/大头/datou1.jpg', 'images/大头/datou2.jpg', 'images/大头/datou3.jpg']
+    images: ['images/大头/datou11.jpg', 'images/大头/datou10.jpg', 'images/大头/datou9.jpg', 'images/大头/datou8.jpg', 'images/大头/datou7.jpg', 'images/大头/datou6.jpg', 'images/大头/datou5.jpg', 'images/大头/datou4.jpg', 'images/大头/datou1.jpg', 'images/大头/datou2.jpg', 'images/大头/datou3.jpg']
   },
   {
     name: '漂亮橘',
