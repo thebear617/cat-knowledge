fc00dde3f16b5f2b6315b389cc863314162edad8
莫开车 <1357953389@qq.com>
2026-08-02 16:03:07 +0800

    chore: 固定本地开发端口

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://thebear617.github.io',
   base: '/cat-knowledge',
+  vite: { server: { strictPort: true } },
   output: 'static'
 });
